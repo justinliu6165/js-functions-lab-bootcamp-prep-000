fd5cba2f6de16cdd39ebe14909950fffcb5dec00
Justin Liu <justin_liu@hotmail.co.uk>
2018-05-04 21:30:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ function happyHolidaysTo(name)
   return "Happy holidays, "+name+"!"
 }
 
-function happyHolidayTo(holiday, names)
+function happyHolidayTo(holiday, name)
 {
-  return "Happy "+holiday+", "+names"!"
+  return 'Happy ${holiday}, ${name}!'
 }
 
 function holidayCountdown(holiday, days)
 {
-  return "It\'s " + days + "days until " + holiday + "!"
+  return 'It\'s ${days} days until ${holiday}!'
 }
